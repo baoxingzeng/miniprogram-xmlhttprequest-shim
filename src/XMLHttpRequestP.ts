1e5f42ed4d8f1a9f8b5ec718c5679c84a96299be
Xingzeng Bao <baoxingzeng@gmail.com>
2026-07-28 10:36:01 +0800

    support textMode

--- src/XMLHttpRequestP.ts
+++ src/XMLHttpRequestP.ts
@@ -217,8 +217,8 @@ export class XMLHttpRequestP extends XMLHttpRequestEventTargetP implements XMLHt
         const request = (function (data?: string | ArrayBuffer) {
             if (requestId !== s.requestId) return;
             if (s.pos !== XHRCycle.LOADSTART && s.pos !== XHRCycle.UPLOAD_LOADEND) return;
-            options.data = data ? coerceBody(data) : undefined;
             options.headers = options.header!;  // Alipay Mini Program
+            options.data = data ? coerceBody(data) : undefined;
             s.requestTask = mp.request(options);
         }).bind(this);
 
